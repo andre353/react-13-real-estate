9e16d0a85bce2bab64fdbcebb7560c401a496705
andre353 <eva595eva@gmail.com>
2022-01-16 19:47:19 +0300

    Create Layout component, update app.js, update Property component to output the rest of the fetched data

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress'; // progress bar
+import { ChakraProvider } from '@chakra-ui/react';
+
+import Layout from '../components/Layout'; // navigation bar, content, footer
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
   
 }
 export default MyApp;
